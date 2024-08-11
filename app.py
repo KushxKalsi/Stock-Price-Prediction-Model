9035733ed53990e3c1866d30ecf530f53892244b
Kush <142648842+KushxKalsi@users.noreply.github.com>
2024-08-11 17:34:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
 import yfinance as yf
-from keras.models import load_model
+import tensorflow as tf
+from tensorflow.keras.models import load_model
 import streamlit as st
 import matplotlib.pyplot as plt
 
